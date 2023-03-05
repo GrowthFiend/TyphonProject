ccd73a885d4cdad073aaee4eeb14f572740bf257
Serg <GrowthFiend@gmail.com>
2023-03-05 15:39:34 +0300

    Теперь если нажать SingleGame меню станет невидимым

--- Scenes/Menu.gd
+++
@@ -1,4 +0,0 @@
-extends Control
-
-func _ready():
-	set_position(Vector2(get_parent().get_parent().size.x/2, get_parent().get_parent().size.y/2))
---
+++ Scripts/Menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	#set_position(Vector2(get_parent().get_parent().size.x/2, get_parent().get_parent().size.y/2))
+	pass
+
+
+func _on_SingleGame_pressed():
+	print("qqq")
+	visible = false
