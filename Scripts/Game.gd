c08a48b09364d92a0253472c8df282a913c9988c
GrowthFiend <GrowthFiend@gmail.com>
2023-03-07 13:48:51 +0300

    Now the turn passes from one player to another

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,16 +1,27 @@
 extends Node2D
 
+const PLAYERS_COUNT = 2
+var m_turn = 0
+
 func _ready():
 	randomize()
 	hand_out_cards()
+	play()
 
 func hand_out_cards():
-	var i = $Table/Deck.size() / 2
+	var i = $Table/Deck.size()
 	while i > 0:
 		var card = $Table/Deck.pop_back()
-		$Player/Hand.push_back(card)
-		$Player/Hand.add_child(card)
-		var card2 = $Table/Deck.pop_back()
-		$Player2/Hand.push_back(card2)
-		$Player2/Hand.add_child(card2)
+		var turn = i%PLAYERS_COUNT
+		var current_player = "Player%s" % turn
+		get_node(current_player).get_node("Hand").push_back(card)
+		get_node(current_player).get_node("Hand").add_child(card)
 		i -= 1
+
+func play():
+	var current_player = "Player%s" % m_turn
+	get_node(current_player).get_node("Turn").visible = true
+
+func change_turn():
+	m_turn = (m_turn+1)%PLAYERS_COUNT
+	play()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,4 +1,5 @@
 extends Node2D
+export var pl_id = 0
 
 func _ready():
 	$Hand.init()
@@ -10,3 +11,5 @@ func _on_Turn_pressed():
 	card.flip()
 	get_parent().get_node("Table/Stake").push_back(card)
 	get_parent().get_node("Table/Stake").add_child(card)
+	$Turn.visible = false
+	get_parent().change_turn()
